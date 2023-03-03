084cad852cadf1e003c1199a2bac6b60d6322053
pedro@valdez <pedro@valdez>
2023-03-03 15:09:14 -0600

    Removed unecessary space

--- lib/apods/fetching.ts
+++ lib/apods/fetching.ts
@@ -8,7 +8,7 @@ const instance = axios.create({
 	 * Since the API only has one endpoint,
 	 * it'll be included in the baseURL.
 	 */
-	baseURL: " https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+	baseURL: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
 })
 
 export async function getMissingApods(): Promise<[Apod] | null> {
