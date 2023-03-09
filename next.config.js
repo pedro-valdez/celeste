dd49585862ba9f9a2a6222eef54ac3c7457e6f6c
pedro@valdez <pedro@valdez>
2023-03-09 17:51:38 -0600

    Added image hostname

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "apod.nasa.gov",
+				port: "",
+				pathname: "/apod/image/**",
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
