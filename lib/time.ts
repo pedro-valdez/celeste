997f828285bb6c6927b4b9335aa82d770e9d0f48
pedro@valdez <pedro@valdez>
2023-03-10 17:17:56 -0600

    Added human readable dates

--- lib/time.ts
+++ lib/time.ts
@@ -10,6 +10,7 @@ dayjs.extend(customParseFormat)
 
 export const yearFormat = "YYYY"
 export const dateFormat = "YYYY-MM-DD"
+const humanFormat = "MMMM D, YYYY"
 export const ONE_DAY = 86400 // one day in seconds
 
 export function getYear(date: string) {
@@ -22,4 +23,15 @@ export function getTodayDate() {
 
 export function isDateValid(date: string) {
 	return dayjs(date, dateFormat, true).isValid()
+}
+
+export function humanDate(date: string): string {
+	const day = dayjs(date).tz()
+	const isToday = day.format(dateFormat) === getTodayDate()
+
+	if (isToday) {
+		return "the Day"
+	}
+
+	return day.format(humanFormat)
 }
