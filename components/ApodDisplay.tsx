413bf55309a0b210ce5c5dc5e5575273e9fa8fcf
pedro@valdez <pedro@valdez>
2023-03-10 18:09:43 -0600

    Added styles for larger screens

--- components/ApodDisplay.tsx
+++ components/ApodDisplay.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 export default function ApodDisplay({ apod }: Props) {
 	return (
-		<div className="space-y-[1em]">
-			<h1 className="mb-[1em]">
+		<div className="space-y-[1em] xl:space-y-0 xl:grid xl:grid-cols-2 xl:grid-rows-[auto_auto_1fr] xl:gap-x-[3em] xl:gap-y-[1em]">
+			<h1 className="mb-[1em] xl:mb-0 2xl:pt-[0.5em]">
 				👽 Astronomy{' '}
 				<br className="2xl:hidden"/>
 				Picture of {humanDate(apod.date)}
@@ -21,14 +21,14 @@ export default function ApodDisplay({ apod }: Props) {
 				{ apod.copyright
 					? <>
 							{' '}
-							<br className="2xl:hidden" />
+							<br />
 							<span className="text-black">by </span>
 							{apod.copyright}
 						</>
 					: <></>
 				}
 			</h2>
-			<p>{ apod.explanation }</p>
+			<p className="xl:max-w-md 2xl:max-w-2xl 2xl:pb-[1em]">{ apod.explanation }</p>
 		</div>
 	)
 }
--- components/ApodMedia.tsx
+++ components/ApodMedia.tsx
@@ -9,12 +9,12 @@ interface Props {
 export default function ApodMedia({ apod }: Props) {
 	if (apod.media_type === "image") {
 		return (
-			<div className="">
+			<div className="relative row-start-1 row-end-4">
 				<Image
 					src={apod.hdurl! || apod.url!}
 					alt={apod.explanation}
 					fill
-					className="!static w-full h-auto rounded-[1.5em]"
+					className="!static w-full h-auto rounded-[1.5em] xl:!absolute xl:h-full xl:object-contain xl:object-right xl:rounded-none xl:py-[1em] 2xl:py-0"
 				/>
 			</div>
 		)
