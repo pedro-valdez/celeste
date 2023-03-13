db2fa023c51998a01f43dd5432f51cd637160499
pedro@valdez <pedro@valdez>
2023-03-13 17:07:44 -0500

    Added props
    next and previous depend on whether props are passed or not.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react"
 import { HiMenu, HiXCircle } from "react-icons/hi"
 
-export default function Navbar() {
+
+interface Props {
+	date?: string,
+}
+
+export default function Navbar({ date }: Props) {
 	const [isOpen, setIsOpen] = useState(false)
 
 	return (
@@ -21,10 +26,16 @@ export default function Navbar() {
 				<ul className="font-bold space-y-2 xl:space-y-0 xl:flex xl:gap-x-6 xl:text-nasa-blue xl:items-center xl:justify-end">
 					<li><span className="underline mr-2">All Apods</span>🔭</li>
 					<li className="xl:text-white xl:px-6 xl:py-1.5 xl:rounded-full xl:bg-nasa-blue"><span className="underline mr-2 xl:no-underline">Calendar</span>📅</li>
-					<div className="space-y-2 xl:fixed xl:right-12 xl:bottom-4 xl:flex xl:gap-x-6 xl:space-y-0">
-						<li><span className="underline mr-2">Previous</span>⬅️</li>
-						<li><span className="underline mr-2">Next</span>➡️</li>
-					</div>
+					{
+						date
+							?
+							<div className="space-y-2 xl:fixed xl:right-12 xl:bottom-4 xl:flex xl:gap-x-6 xl:space-y-0">
+								<li><span className="underline mr-2">Previous</span>⬅️</li>
+								<li><span className="underline mr-2">Next</span>➡️</li>
+							</div>
+							:
+							<></>
+					}
 				</ul>
 
 				<HiXCircle
