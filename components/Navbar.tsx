4526fda4907bd03e5b93f592509c622141dd72d4
pedro@valdez <pedro@valdez>
2023-03-13 16:16:52 -0500

    Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { HiMenu, HiXCircle } from "react-icons/hi"
+
+export default function Navbar() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	return (
+		<div className={`fixed xl:static
+			right-4 bottom-4 xl:left-0 xl:top-0
+			p-4
+			bg-nasa-blue text-white xl:bg-white xl:text-black
+			${isOpen ? "rounded-2xl" : "rounded-full"} xl:rounded-none`}>
+
+			<HiMenu
+				onClick={() => setIsOpen(true)}
+				className={`${isOpen ? "hidden" : "block"} xl:hidden`}
+			/>
+
+			<nav className={`${isOpen ? "block" : "hidden"} xl:block
+				text-right`}>
+				<ul className="font-bold space-y-2 xl:space-y-0 xl:flex xl:gap-x-6 xl:text-nasa-blue xl:items-center xl:justify-end">
+					<li><span className="underline mr-2">All Apods</span>🔭</li>
+					<li className="xl:text-white xl:px-6 xl:py-1.5 xl:rounded-full xl:bg-nasa-blue"><span className="underline mr-2 xl:no-underline">Calendar</span>📅</li>
+					<div className="space-y-2 xl:fixed xl:right-12 xl:bottom-4 xl:flex xl:gap-x-6 xl:space-y-0">
+						<li><span className="underline mr-2">Previous</span>⬅️</li>
+						<li><span className="underline mr-2">Next</span>➡️</li>
+					</div>
+				</ul>
+
+				<HiXCircle
+					onClick={() => setIsOpen(false)}
+					className="mt-2 ml-auto text-xl xl:hidden"
+				/>
+			</nav>
+		</div>
+	)
+}
