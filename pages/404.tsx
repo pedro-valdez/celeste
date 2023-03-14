94ed95d7e6b4af1c623a378ce06881cf72a547a6
pedro@valdez <pedro@valdez>
2023-03-13 23:03:13 -0500

    Added error page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar"
+
+
+export default function CustomError() {
+	return (
+		<div className="px-4 pt-8 pb-16 sm:px-8 xl:pt-0 xl:pb-8">
+			<Navbar />
+			<div className="h-[calc(100vh-8rem)] flex items-center justify-center">
+				<div className="my-auto">
+					<h1 className="text-nasa-red">Error</h1>
+					<p>Some dates don't have an entry.</p>
+				</div>
+			</div>
+		</div>
+	)
+}
