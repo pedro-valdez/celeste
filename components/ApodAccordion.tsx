798f6442b479465066bfbaab0c87f32654a0a296
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-03-16 16:34:34 -0500

    Restructure of HTML
    Also made component more visually appealing

--- components/ApodAccordion.tsx
+++ components/ApodAccordion.tsx
@@ -1,4 +1,5 @@
 import { Apod } from "@/lib/apods/apods"
+import { humanDate } from "@/lib/time"
 import { useState } from "react"
 import ApodDisplay from "./ApodDisplay"
 
@@ -11,14 +12,25 @@ export default function Accordion({ apod } : Props) {
 	const [isOpen, setIsOpen] = useState(false)
 
 	return (
-		<li>
-			<article onClick={() => setIsOpen(!isOpen)}>
-				{
-					isOpen
-						? <ApodDisplay apod={apod}/>
-						: <h1>{apod.date}</h1>
-				}
-			</article>
-		</li>
+		<article>
+			<div
+				className="flex justify-between text-2xl sm:text-3xl 2xl:text-4xl border-b-2 border-nasa-blue pb-4 xl:pb-8"
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				<span className="font-bold text-nasa-blue">{humanDate(apod.date)}</span>
+				<span>
+					{
+						isOpen
+							? "❌"
+							: "⬇️"
+					}
+				</span>
+			</div>
+			{
+				isOpen
+					? <ApodDisplay apod={apod}/>
+					: <></>
+			}
+		</article>
 	)
 }
