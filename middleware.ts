0b5b756dc258e816883fd8adaf72731bf33fbfc6
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-06-20 02:37:17 -0500

    Better pattern matching for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,35 @@
-import { NextResponse } from "next/server"
+import { NextResponse, URLPattern } from "next/server"
 import type { NextRequest } from "next/server"
-import { isDateInRange, isDateValid } from "./lib/apods/time"
+import { getTodayDate, isDateInRange, isDateValid } from "./lib/apods/time"
+
+const PATTERNS = {
+	HOME: new URLPattern({ pathname: "/" }),
+	APOD_AT_DATE: new URLPattern({ pathname: "/apods/:date*" }),
+}
 
 export function middleware(request: NextRequest) {
-	const date = request.nextUrl.pathname.split("/").at(-1)
+	const { pathname } = request.nextUrl
+	const segments = pathname.split("/")
+
+	const redirectToLatest = NextResponse.redirect(new URL(`/apods/${getTodayDate()}`, request.url))
 	const responseRedirectHome = NextResponse.redirect(new URL("/", request.url))
 
-	if (!date) { return responseRedirectHome }
+	const isHomePath = PATTERNS.HOME.test({ pathname })
+	if (isHomePath) { return redirectToLatest }
 
-	const isDateCorrect = isDateValid(date) && isDateInRange(date)
 
-	if (!isDateCorrect) { return responseRedirectHome }
+	const isApodAtDatePath = PATTERNS.APOD_AT_DATE.test({ pathname })
+	if (isApodAtDatePath) {
+		// If pathaname passes pattern test, then date is asserted to exist.
+		const date = segments.at(-1)!
+
+		const isDateCorrect = isDateValid(date) && isDateInRange(date)
+
+		if (!isDateCorrect) { return responseRedirectHome }
+	}
+
 }
 
 export const config = {
-	matcher: "/apods/:date*",
+	matcher: ["/apods/:date*", "/"],
 }
